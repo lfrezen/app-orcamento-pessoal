0dd8315c59c297e8c6faf98dfc5181d0da1324c4
Lucian Rezende <lucianfiocello@gmail.com>
2020-12-17 16:52:30 -0300

    Limpando formulário ao salvar e ajustes do Bootstrap 5

--- app.js
+++ app.js
@@ -34,6 +34,19 @@ class Bd {
         localStorage.setItem(id, JSON.stringify(d));
         localStorage.setItem('id', id);
     }
+
+    recuperarTodosRegistros() {
+        let despesas = Array();
+        let id = localStorage.getItem('id');
+        for(let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i));
+            if (despesa === null) {
+                continue
+            }
+            despesas.push(despesa);
+        }
+        return despesas;
+    }
 }
 
 let bd = new Bd();
@@ -62,11 +75,48 @@ function cadastrarDespesa() {
         $('#messageTitle').addClass('text-success');
         $(':button:contains(Voltar)').addClass('btn-success');
         $('#modalRegistraDespesa').modal('show');
+
+        ano.value = "";
+        mes.value = "";
+        dia.value = "";
+        tipo.value = "";
+        descricao.value = "";
+        valor.value = "";
     } else {
         document.getElementById('exampleModalLabel').innerText = 'Erro na gravação';
-        document.getElementById('messageBody').innerText = 'Existem campos obrigatórios que não foram preenchidos';        
-        $('#messageTitle').addClass('text-danger');        
+        document.getElementById('messageBody').innerText = 'Existem campos obrigatórios que não foram preenchidos';
+        $('#messageTitle').addClass('text-danger');
         $(':button:contains(Voltar)').addClass('btn-danger');
         $('#modalRegistraDespesa').modal('show');
     }
+}
+
+function carregaListaDespesas() {
+    let despesas = Array();
+
+    despesas = bd.recuperarTodosRegistros();
+
+    let listaDespesas = document.getElementById('listaDespesas');
+
+    despesas.forEach(function(d) {
+        
+        let linha = listaDespesas.insertRow();
+
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+            break
+            case '2': d.tipo = 'Educação'
+            break
+            case '3': d.tipo = 'Lazer'
+            break
+            case '4': d.tipo = 'Saúde'
+            break
+            case '5': d.tipo = 'Transporte'
+            break
+        }
+        linha.insertCell(1).innerHTML = d.tipo;
+        linha.insertCell(2).innerHTML = d.descricao;
+        linha.insertCell(3).innerHTML = d.valor;
+    })
 }
