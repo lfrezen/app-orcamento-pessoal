87814f9a3acd40f772336b9e74ddd37a03b7dc5f
Lucian Rezende <lucianfiocello@gmail.com>
2020-12-17 18:06:50 -0300

    Tratando a consulta com filtros

--- app.js
+++ app.js
@@ -47,6 +47,32 @@ class Bd {
         }
         return despesas;
     }
+
+    pesquisar(despesa) {
+        let despesasFiltradas = Array();
+        despesasFiltradas = this.recuperarTodosRegistros();
+        console.log(despesa);
+        console.log(despesasFiltradas);
+        if (despesa.ano != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+        }
+        if (despesa.mes != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        }
+        if (despesa.dia != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+        }
+        if (despesa.tipo != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+        }
+        if (despesa.descricao != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+        }
+        if (despesa.valor != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+        }
+        return despesasFiltradas;
+    }
 }
 
 let bd = new Bd();
@@ -91,12 +117,14 @@ function cadastrarDespesa() {
     }
 }
 
-function carregaListaDespesas() {
-    let despesas = Array();
+function carregaListaDespesas(despesas = Array(), filtro = false) {
 
-    despesas = bd.recuperarTodosRegistros();
+    if (despesas.length == 0 && filtro == false) {
+        despesas = bd.recuperarTodosRegistros();  
+    }
 
     let listaDespesas = document.getElementById('listaDespesas');
+    listaDespesas.innerHTML = '';
 
     despesas.forEach(function(d) {
         
@@ -119,4 +147,19 @@ function carregaListaDespesas() {
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
     })
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value;
+    let mes = document.getElementById('mes').value;
+    let dia = document.getElementById('dia').value;
+    let tipo = document.getElementById('tipo').value;
+    let descricao = document.getElementById('descricao').value;
+    let valor = document.getElementById('valor').value;
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
+
+    let despesas = bd.pesquisar(despesa);
+
+    carregaListaDespesas(despesas, true);
 }
